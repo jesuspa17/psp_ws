3a578a77bb3ef7677b6fb672cf9a22486ea99978
jesuspa17 <jpallares94.jp@gmail.com>
2015-10-20 17:28:01 +0200

    Update MainActivity.java

--- Tema01_Programación_Multihilo_Android/Ejercicio02_EjercicioCarrera/app/src/main/java/com/salesianostriana/psp/tema02_ejerciciocarrera/MainActivity.java
+++ Tema01_Programación_Multihilo_Android/Ejercicio02_EjercicioCarrera/app/src/main/java/com/salesianostriana/psp/tema02_ejerciciocarrera/MainActivity.java
@@ -5,6 +5,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+(2) Carrera olímpica
+
+En este ejercicio vamos a realizar un experimento: si lanzamos 8 hilos que realizan la misma tarea, 
+y el planificador le asigna el mismo tiempo de ejecución a cada uno, debería finalizar primero el primer 
+hilo que se lanzó, y así sucesivamente, ¿no?. Lo comprobaremos de la siguiente forma: supongamos 
+que tendremos una carrera del gran premio de atletismo (la de 1500 metros).
+
+Tendremos que crear la clase Corredor, que tendrá que implementar un bucle que cuente desde 0 a 1500 
+(una iteración por metro recorrido). Cuando finalice el bucle, deberá mostrar un mensaje 
+por pantalla indicando el nombre del corredor y que ha finalizado (El corredor Fermín Cacho ha finalizado la prueba).
+
+NOTA: Si los corredores realizan la carrera muy rápido, se puede introducir, dentro del bucle, 
+un pequeño lapso de tiempo (10 ms) de sueño, mediante el uso del método sleep.
+
+NOTA 2: Para poder proporcionarle a cada corredor un nombre, podemos añadir a la 
+clase Corredor un constructor que reciba dicho nombre como parámetro.
+
+ * */
+
 public class MainActivity extends Activity {
 
     @Override
